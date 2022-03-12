3616fbf7900a63231ecd6b844db662d41dde52a8
Augusto Cesar <acs-03@hotmail.com>
2022-03-11 22:15:40 -0300

    Project created

---
+++ src/test/java/com/devsuperior/dsposts/DspostsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsposts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
