c26ca4c996136284b755d733d029c2422e1ee020
Augusto Cesar <acs-03@hotmail.com>
2022-03-15 22:35:50 -0300

    GET user by id

--- src/main/java/com/devsuperior/dsposts/controllers/UserController.java
+++ src/main/java/com/devsuperior/dsposts/controllers/UserController.java
@@ -8,6 +8,7 @@ import com.devsuperior.dsposts.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,12 @@ public class UserController {
     public ResponseEntity<List<UserDTO>> findAll() {
         List<UserDTO> users = service.findAll();
         return ResponseEntity.ok().body(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        UserDTO user = service.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 
 }
--- src/main/java/com/devsuperior/dsposts/services/UserService.java
+++ src/main/java/com/devsuperior/dsposts/services/UserService.java
@@ -1,11 +1,13 @@
 package com.devsuperior.dsposts.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import com.devsuperior.dsposts.dto.UserDTO;
 import com.devsuperior.dsposts.entities.User;
 import com.devsuperior.dsposts.repositories.UserRepository;
+import com.devsuperior.dsposts.services.exceptions.ResourceNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,6 +21,12 @@ public class UserService {
     public List<UserDTO> findAll() {
         List<User> users = repository.findAll();
         return users.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+    }
+
+    public UserDTO findById(String id) {
+        Optional<User> user = repository.findById(id);
+        User entity = user.orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado"));
+        return new UserDTO(entity);
     }
 
 }
